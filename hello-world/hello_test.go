df0ab96411cb458c46f3dee9ba1349ba35100c9a
Luís Pinto <luispinto23@gmail.com>
2022-02-24 11:34:27 +0000

    refactor: hello function now receives a name and composes a new message

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	got := Hello("Luís")
-	expected := "Hello, Luís!"
+	expected := "Hello, Luís"
 
 	if got != expected {
 		t.Errorf("got %q, want %q", got, expected)
