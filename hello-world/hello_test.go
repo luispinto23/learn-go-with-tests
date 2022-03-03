45d503676f6e4ac4a122ba5594a43f11c877b133
Luís Pinto <luispinto23@gmail.com>
2022-03-03 11:27:25 +0000

    test: added test case for spanish greeting

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -11,14 +11,20 @@ func TestHello(t *testing.T) {
 		}
 	}
 
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Luís")
+	t.Run("say hello in spanish", func(t *testing.T) {
+		got := Hello("Luís", "Spanish")
+		want := "Hola, Luís"
+
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say hello in english", func(t *testing.T) {
+		got := Hello("Luís", "")
 		want := "Hello, Luís"
 
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, world"
 
 		assertCorrectMessage(t, got, want)
