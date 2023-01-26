c1a554a9a3b36f0514e00303cda8dca1acf7cc22
Luís Pinto <luispinto23@gmail.com>
2023-01-26 19:02:06 +0000

    feat: add repeat example

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -1,6 +1,9 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
 	repeated := Repeat("a", 2)
@@ -9,6 +12,12 @@ func TestRepeat(t *testing.T) {
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("x", 6)
+	fmt.Println(repeated)
+	// Output: xxxxxx
 }
 
 func BenchmarkRepeat(b *testing.B) {
