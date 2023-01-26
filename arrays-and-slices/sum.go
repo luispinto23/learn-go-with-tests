a6fed32136fdc9c72f2faf7e72730f4f82cfa7a2
Luís Pinto <luispinto23@gmail.com>
2023-01-26 23:42:09 +0000

    refactor: refactor SumAll

--- arrays-and-slices/sum.go
+++ arrays-and-slices/sum.go
@@ -11,11 +11,10 @@ func Sum(numbers []int) int {
 }
 
 func SumAll(numbersToSum ...[]int) []int {
-	numbersLength := len(numbersToSum)
-	sums := make([]int, numbersLength)
+	var sums []int
 
-	for i, numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
 	}
 
 	return sums
