1e3c203b6f20d669a6ffd1ab565d9332269a7f92
Luís Pinto <luispinto23@gmail.com>
2023-01-30 18:15:28 +0000

    refactor: add sleeper

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -7,17 +7,36 @@ import (
 	"time"
 )
 
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
 const countdownStart = 3
 const finalWord = "Go!"
 
-func Countdown(out io.Writer) {
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, fmt.Sprint(i))
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 	}
 	fmt.Fprint(out, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
