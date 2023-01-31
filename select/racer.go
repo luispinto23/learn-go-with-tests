3938e76c398181f58f98b511a9b4c5f317f1ba53
Luís Pinto <luispinto23@gmail.com>
2023-01-31 23:08:22 +0000

    refactor: make a configurable racer

--- select/racer.go
+++ select/racer.go
@@ -6,13 +6,27 @@ import (
 	"time"
 )
 
+// SELECT
+// - Helps you wait on multiple channels.
+// - Sometimes you'll want to include time.After in one of your cases to prevent your system blocking forever.
+
+// httptest
+// - A convenient way of creating test servers so you can have reliable and controllable tests.
+// - Uses the same interfaces as the "real" net/http servers which is consistent and less for you to learn.
+
+var tenSecondTimeout = 10 * time.Second
+
 func Racer(a, b string) (winner string, error error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
+}
+
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, error error) {
 	select {
 	case <-ping(a):
 		return a, nil
 	case <-ping(b):
 		return b, nil
-	case <-time.After(10 * time.Second):
+	case <-time.After(timeout):
 		return "", errors.New("time out")
 	}
 }
